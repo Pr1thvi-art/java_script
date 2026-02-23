f8b8c5a2c611d3196ca1768418b25380311961b9
Pr1thvi-art <chavanprithvi46@gmail.com>
2026-02-23 19:20:44 +0000

    container config

---
+++ 01_basics/03_coversionsOperation.js
@@ -0,0 +1,37 @@
+let score;
+
+// console.log (typeof score)
+
+let valueInNumber = Number(score)
+ // console.log (typeof valueInNumber)
+ //console.log (valueInNumber)
+// when we convert
+// 33 => number
+// 33abc => Nan
+// true => 1; false=> 0
+let IsLoggedIn = 1
+let booleanIsLoggedIn = Boolean (IsLoggedIn)
+// console.log (booleanIsLoggedIn)
+
+let someNumber = 45
+let stringNumber = String(someNumber)
+//  console.log (typeof stringNumber)
+
+let value = 4
+let negValue = -value 
+// console.log(negValue)
+
+// console.log (2+2)
+// console.log(2_2)
+// console.log(2*2)
+// console.log(2%2)
+// console.log(2**3)
+
+let c1 = "hii"
+let c2 = " prithvi" 
+let c3 = c1 + c2 
+// console.log(c3) 
+
+let Gamecounter = 100
+++Gamecounter;
+//console.log(Gamecounter)
---
+++ 01_basics/comparision.js
@@ -0,0 +1,8 @@
+// console.log (2>2)
+// console.log (2==2)
+// console.log ("2"==2)
+// console.log("2"===2)
+
+let c = true
+// console.log(1===c)
+
